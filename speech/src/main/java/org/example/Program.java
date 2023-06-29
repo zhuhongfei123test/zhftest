4d3d3bcb52c22dcda3d9acfd0f31df76b4e62a7f
zhuhongfei123test <z15175303556@163.com>
2023-06-29 13:53:34 +0800

    Initial commit

---
+++ speech/src/main/java/org/example/Program.java
@@ -0,0 +1,25 @@
+package main.java.org.example;
+
+import com.microsoft.cognitiveservices.speech.*;
+import com.microsoft.cognitiveservices.speech.audio.AudioConfig;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class Program {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        SpeechConfig speechConfig = SpeechConfig.fromSubscription("724ceab8c26a4dc6b5a8095c01686db6", "chinaeast2");
+        speechConfig.setSpeechRecognitionLanguage("zh-cn");
+        speechConfig.enableDictation();
+        fromMic(speechConfig);
+    }
+
+    public static void fromMic(SpeechConfig speechConfig) throws InterruptedException, ExecutionException {
+        AudioConfig audioConfig = AudioConfig.fromDefaultMicrophoneInput();
+        SpeechRecognizer speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+        System.out.println("Speak into your microphone.");
+        Future<SpeechRecognitionResult> task = speechRecognizer.recognizeOnceAsync();
+        SpeechRecognitionResult result = task.get();
+        System.out.println("RECOGNIZED: Text=" + result.getText());
+    }
+}
